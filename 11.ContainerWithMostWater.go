b59daa49fc0eb0b23bd29f7f5f09eae35c91955d
Praveen Kumar Ramineni <prramineni@paypal.com>
2024-04-24 19:34:04 -0700

    pb 11

--- 11.ContainerWithMostWater.go
+++ 11.ContainerWithMostWater.go
@@ -8,19 +8,12 @@ import "math"
 
 func maxArea(height []int) int {
 	l, r := 0, len(height)-1
-	minVol := float64(0)
+	maxVol := float64(0)
 
 	for l < r {
 		vol := math.Min(float64(height[l]), float64(height[r])) * float64(r-l)
-		if vol > minVol {
-			minVol = math.Max(minVol, vol)
-		}
-
-		if height[r] > height[l] {
-			l++
-		} else {
-			r--
-		}
+		maxVol = math.Max(maxVol, vol)
 	}
-	return int(minVol)
+
+	return int(maxVol)
 }
